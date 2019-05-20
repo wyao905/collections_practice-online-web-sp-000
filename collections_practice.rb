e1565b644e51d0dfd3939e2cbb2fb50c47486d4f
Warren Yao <warrenyca905@gmail.com>
2019-05-20 18:06:30 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -56,5 +56,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect { |word, index| word << "s" if index != 1}
+  array.each_with_index.collect { |word, index| word += "s" if index != 1}
 end
