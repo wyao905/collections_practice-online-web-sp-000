b608596e400e2d105082bf647a6a31ef4f77fa10
Warren Yao <warrenyca905@gmail.com>
2019-05-20 17:25:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,34 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a  == b
+      0
+    elsif a > b
+      -1
+    else
+      1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.downcase <=> b.downcase
+  end
+end
+
+def swap_elements(array)
+  temp = array[2]
+  array[2] = array[1]
+  array[1] = temp
+  return array
+end
+
+def reverse_array(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
